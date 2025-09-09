eb74b52fa880c5fee3510300d660b8d3ed65ab8f
124048712-dotcom <124048712@upq.edu.mx>
2025-09-09 15:38:08 -0600

    Creación de Hola mundo, uso de html, java, tipo de datos, uso de consola

---
+++ JS Para Principiantes/app.js
@@ -0,0 +1,28 @@
+console.log('this is a string');
+
+
+/*
+//tipos de datos
+"Hello world" //String
+'Hello world' //String
+
+1000000 //Number
+-2.3   //Numbre
+
+//Boolean
+true 
+false 
+
+//array
+['joe','ryan','martha']
+[1,2,3]
+[true,false,true,false]
+
+//object
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+} */
