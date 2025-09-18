a82c783fe4c7a1d0bedc599984a665a7bf213601
124048712-dotcom <124048712@upq.edu.mx>
2025-09-17 20:34:44 -0600

    Se cambio el var por let y const

---
+++ Practica1/ejercicioA.js
@@ -0,0 +1,9 @@
+//BELLO BOBADILLA BARBARA ITZEL
+let nombre = "Armando";
+const edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+//muestra en consola el contenido de saludo
+console.log(saludo); 
