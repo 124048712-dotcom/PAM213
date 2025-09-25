0ce66be0d4a12176760c986f26223e0509613047
124048712-dotcom <124048712@upq.edu.mx>
2025-09-25 15:16:16 -0600

    se creo una funcion que simula una llamada a una Api, use setTimeout y async

---
+++ Practica3/ejercicioC.js
@@ -0,0 +1,19 @@
+//BELLO BOBADILLA BARBARA ITZEL 
+function simularPeticionAPI() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Datos recibidos correctamente");
+    }, 5000); 
+  });
+}
+
+
+async function obtenerDatos() {
+  console.log("Espera el resultado...");
+  const resultado = await simularPeticionAPI(); 
+  console.log(resultado); 
+}
+
+
+obtenerDatos();
+
